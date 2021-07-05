591974e2a8a0f289301d99a15725b3096ee4df52
Willyan Ferdinando <willyantferdinando@gmail.com>
2021-07-04 23:37:18 -0300

    Add dependen destroy

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,6 @@
 class Content < ApplicationRecord
   belongs_to :user
-  has_many :tag_contents
+  has_many :tag_contents, dependent: :destroy
   has_many :tags, through: :tag_contents
 
   validates :title, :description, presence: true
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
   belongs_to :user
-  has_many :tag_contents
+  has_many :tag_contents, dependent: :destroy
   has_many :contents, through: :tag_contents
 
   validates :name, presence: true, uniqueness: { scope: :user_id }
