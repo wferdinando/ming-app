750f1d0945e55bdbb9b213c8797a987139bca7d6
Willyan Ferdinando <willyantferdinando@gmail.com>
2021-07-04 23:19:54 -0300

    Add Model TagContent

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,7 @@
 class Content < ApplicationRecord
   belongs_to :user
+  has_many :tag_contents
+  has_many :tags, through: :tag_contents
 
   validates :title, :description, presence: true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,7 @@
 class Tag < ApplicationRecord
   belongs_to :user
+  has_many :tag_contents
+  has_many :contents, through: :tag_contents
 
   validates :name, presence: true, uniqueness: { scope: :user_id }
 end
