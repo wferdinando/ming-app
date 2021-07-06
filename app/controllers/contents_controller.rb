c96fe8d801de28b77fe16272e4bc250bc2693842
Willyan Ferdinando <willyantferdinando@gmail.com>
2021-07-05 22:56:18 -0300

    Add View Edit Content

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -8,4 +8,22 @@ class ContentsController < ApplicationController
   def new
     @content = Content.new
   end
+
+  def create
+    @content = current_user.contents.build(content_params)
+    if @content.save
+      redirect_to contents_path, notice: 'Content successfully created!'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @content = Content.find(params[:id])
+  end
+
+  private
+  def content_params
+    params.require(:content).permit(:title, :description)
+  end
 end
