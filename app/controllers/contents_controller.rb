7dfba03baf71795f0a8b0e0c805d9ece276dd18f
Willyan Ferdinando <willyantferdinando@gmail.com>
2021-07-06 22:26:11 -0300

    Permite adicionar tags no conteudo

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -16,6 +16,7 @@ class ContentsController < ApplicationController
   def create
     @content = current_user.contents.build(content_params)
     if @content.save
+      associate_tags!
       redirect_to contents_path, notice: "Content successfully created!"
     else
       render :new
@@ -27,6 +28,8 @@ class ContentsController < ApplicationController
 
   def update
     if @content.update(content_params)
+      associate_tags!
+
       redirect_to contents_path, notice: "Content successfully uddated!"
     else
       render :edit
@@ -46,5 +49,16 @@ class ContentsController < ApplicationController
 
   def content_params
     params.require(:content).permit(:title, :description)
+  end
+
+  def tags_params
+    params.require(:content).permit(tags: [])[:tags].reject(&:blank?)
+  end
+
+  def associate_tags!
+    tags = tags_params.map do |tag_name|
+      current_user.tags.where(name: tag_name).first_or_initialize
+    end
+    @content.tags = tags
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,17 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import $ from 'jquery'
+import 'select2'
+
+$(document).on('turbolinks:load', function () {
+  $(".select2").select2({
+    tags: true,
+    tokenSeparators: [',', ' ']
+  })
+})
