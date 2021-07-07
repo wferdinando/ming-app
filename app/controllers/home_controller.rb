fce8fd520696266191a34415db7700cbd6450e94
Willyan Ferdinando <willyantferdinando@gmail.com>
2021-07-07 00:04:52 -0300

    Exibe tags do usuario na pagina principal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!    
   def index
-
+    @tags_data = current_user.contents.joins(:tags).group('tags.name').count
   end
 end
